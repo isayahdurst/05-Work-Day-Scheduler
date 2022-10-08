2732d32395ff23ca29f48484382f5bf009ddff31
isayah@fungeapp.com <isayah@fungeapp.com>
2022-10-08 15:32:47 -0700

    Timeslots are now color coded based on the current time and whether a task is in the past, present, or future relative to that time. These colors are automatically updated every second to check whether or not the time has changed to a new hour. Next step is to save each task that a user enters to local storage and load the task upon the page refreshing.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,11 @@
 
 const DateTime = luxon.DateTime;
 
-const now = DateTime.now();
-const year = now.year;
-const month = now.month;
-const day = now.day;
-
-const currentDateField = $("#currentDay");
+// const now = DateTime.now();
+// const year = now.year;
+// const month = now.month;
+// const day = now.day;
+// const hour = now.hour;
 
 const monthNames = new Map([
   [1, "January"],
@@ -24,6 +23,52 @@ const monthNames = new Map([
   [12, "December"],
 ]);
 
-const currentDay = `${monthNames.get(month)} ${day}, ${year}`;
-console.log(currentDay);
-currentDateField.text = currentDay;
+const clock = {
+  updateClock() {
+    setInterval(() => {
+      const now = DateTime.now();
+      this.year = now.year;
+      this.month = now.month;
+      this.day = now.day;
+      this.hour = now.hour;
+      this.currentDay = `${monthNames.get(this.month)} ${this.day}, ${this.year}`;
+      setBackgroundColors();
+      $('#currentDay').text(clock.currentDay);
+      }, 1000);
+  },
+  init() {
+    this.day = DateTime.now().day;
+    this.month = DateTime.now().month;
+    this.year = DateTime.now().year;
+    this.hour = DateTime.now().hour;
+  }
+
+}
+
+const saveTask = function (task) {
+  
+}
+
+const setBackgroundColors = function () {
+  const times = [$('#9am'), $('#10am'), $('#11am'),$('#12pm'),$('#1pm'),$('#2pm'),$('#3pm'),$('#4pm'), $('#5pm')];
+  for (let time of times) {
+    let hourEl = Number(time.attr('data-time'));
+    if (hourEl < clock.hour) {
+      time.addClass('bg-light');
+    } else if (hourEl === clock.hour) {
+      time.addClass('bg-danger');
+    } else if (hourEl > clock.hour) {
+      time.addClass('bg-success');
+    }
+  } 
+}
+
+clock.init();
+setBackgroundColors();
+clock.updateClock();
+
+$('button').on('click', function (event) {
+  console.log(event);
+  console.log(event.target)
+})
+
