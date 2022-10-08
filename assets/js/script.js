711917a91d2ce9e835fd2e83892bbd07aff61be1
MAYC 6976 <isayah@fungeapp.com>
2022-10-08 11:03:49 -0700

    Formatted columns to look like mock-up. Tested JS functionality. Created Month Map to quickly convert from Luxon month # to a string date.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,29 @@
 "use strict";
 
-const dateTime = luxon.DateTime;
+const DateTime = luxon.DateTime;
 
-const now = dateTime.now();
+const now = DateTime.now();
+const year = now.year;
+const month = now.month;
+const day = now.day;
 
-console.log(now.toString());
+const currentDateField = $("#currentDay");
+
+const monthNames = new Map([
+  [1, "January"],
+  [2, "February"],
+  [3, "March"],
+  [4, "April"],
+  [5, "May"],
+  [6, "June"],
+  [7, "July"],
+  [8, "August"],
+  [9, "September"],
+  [10, "October"],
+  [11, "November"],
+  [12, "December"],
+]);
+
+const currentDay = `${monthNames.get(month)} ${day}, ${year}`;
+console.log(currentDay);
+currentDateField.text = currentDay;
