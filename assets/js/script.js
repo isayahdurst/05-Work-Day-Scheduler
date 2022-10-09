ae56a8bde5adfecb5075dde8378c18bba78f3754
MAYC 6976 <isayah@fungeapp.com>
2022-10-09 11:11:36 -0700

    added save/load functionality. Current implementation works in a very basic form, but has a major drawback: tasks saved on one day will load even when the calandar switches to another day. Ideally, tasks should save only for the day they're planned and should clear the screen when a new day begins. They should still be saved in local storage, however, perhaps as their own object and all objeccts saved in local storage can be organized by their date. I will be working on this feature next.

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,30 @@ const monthNames = new Map([
   [12, "December"],
 ]);
 
+const dataToIDConverter = {
+  9: "9am",
+  10: "10am",
+  11: "11am",
+  12: "12pm",
+  13: "1pm",
+  14: "2pm",
+  15: "3pm",
+  16: "4pm",
+  17: "5pm",
+};
+
+const times = [
+  $("#9am"),
+  $("#10am"),
+  $("#11am"),
+  $("#12pm"),
+  $("#1pm"),
+  $("#2pm"),
+  $("#3pm"),
+  $("#4pm"),
+  $("#5pm"),
+];
+
 const clock = {
   updateClock() {
     setInterval(() => {
@@ -31,44 +55,59 @@ const clock = {
       this.month = now.month;
       this.day = now.day;
       this.hour = now.hour;
-      this.currentDay = `${monthNames.get(this.month)} ${this.day}, ${this.year}`;
-      setBackgroundColors();
-      $('#currentDay').text(clock.currentDay);
-      }, 1000);
+      this.currentDay = `${monthNames.get(this.month)} ${this.day}, ${
+        this.year
+      }`;
+      setBackgroundColors(times);
+      $("#currentDay").text(clock.currentDay);
+    }, 1000);
   },
   init() {
     this.day = DateTime.now().day;
     this.month = DateTime.now().month;
     this.year = DateTime.now().year;
     this.hour = DateTime.now().hour;
-  }
+  },
+};
 
-}
-
-const saveTask = function (task) {
-  
-}
-
-const setBackgroundColors = function () {
-  const times = [$('#9am'), $('#10am'), $('#11am'),$('#12pm'),$('#1pm'),$('#2pm'),$('#3pm'),$('#4pm'), $('#5pm')];
+const setBackgroundColors = function (times) {
   for (let time of times) {
-    let hourEl = Number(time.attr('data-time'));
+    let hourEl = Number(time.attr("data-time"));
     if (hourEl < clock.hour) {
-      time.addClass('bg-light');
+      time.addClass("bg-light");
     } else if (hourEl === clock.hour) {
-      time.addClass('bg-danger');
+      time.addClass("bg-danger");
     } else if (hourEl > clock.hour) {
-      time.addClass('bg-success');
+      time.addClass("bg-success");
     }
-  } 
-}
+  }
+};
+
+const saveTask = function (taskNumber) {
+  const task = $(`#${dataToIDConverter[taskNumber]}`).val();
+  localStorage.setItem(taskNumber, task);
+  console.log(localStorage);
+};
+
+const loadTasks = function () {
+  const indexTable = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    indexTable.push(localStorage.key(i));
+  }
+  for (let time of times) {
+    if (indexTable.includes(time.attr("data-time"))) {
+      time.val(localStorage.getItem(time.attr("data-time")));
+    }
+  }
+};
 
 clock.init();
-setBackgroundColors();
+setBackgroundColors(times);
 clock.updateClock();
 
-$('button').on('click', function (event) {
-  console.log(event);
-  console.log(event.target)
-})
+$("button").on("click", function (event) {
+  const taskNumber = $(event.target).attr("data-time");
+  saveTask(taskNumber);
+});
 
+loadTasks();
